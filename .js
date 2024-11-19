40d068148d7dd655864265d754151b2707b8d135
JoaoVitorx11 <0liv3ira397@gmail.com>
2024-11-19 13:29:58 -0300

    atividade web II

---
+++ .js
@@ -0,0 +1,24 @@
+// Função para somar dois números
+function somar(a, b) {
+    return a + b;
+}
+
+// Função para subtrair dois números
+function subtrair(a, b) {
+    return a - b;
+}
+
+// Função para multiplicar dois números
+function multiplicar(a, b) {
+    return a * b;
+}
+
+// Função para dividir dois números
+function dividir(a, b) {
+    if (b === 0) {
+        return "Erro: Divisão por zero!";
+    }
+    return a / b;
+}
+
+// Função principal para ex
